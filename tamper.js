9e5904385ad8850b3ed8a23999ddbdc886decd5d
hejinlin <769316680@qq.com>
2023-09-20 22:06:36 +0800

    feat: init

--- tamper.js
+++ tamper.js
@@ -1,1 +1,14 @@
-console.log("tamper");
+// ==UserScript==
+// @name        Text
+// @namespace   https://github.com/jin-lin0/tampermonkey-demo
+// @match       *://*
+// @grant       none
+// @version     0.1
+// @author      -
+// @description 2023/9/20 21:55:23
+// ==/UserScript==
+
+(function () {
+  "use strict";
+  console.log("tamper");
+})();
