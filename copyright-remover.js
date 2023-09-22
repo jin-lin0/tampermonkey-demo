8426f6b6edb40a044266d01af5f27401a1010b19
hejinlin <769316680@qq.com>
2023-09-22 22:02:47 +0800

    feat: match

--- copyright-remover.js
+++ copyright-remover.js
@@ -1,12 +1,18 @@
 // ==UserScript==
-// @name         掘金剪贴板版权信息去除
+// @name         剪贴板版权信息去除
 // @namespace    https://github.com/jin-lin0/tampermonkey-demo
-// @version      0.2
+// @version      0.3
 // @description  2023/9/22 14:34:30
 // @author       logyes
 // @license      MPL-2.0
 
 // @match        *://juejin.cn/*
+// @match        *://leetcode.cn/*
+// @match        *://*.jianshu.com/*
+// @match        *://*.csdn.net/*
+// @match        *://*.zhihu.com/*
+// @match        *://*.nowcoder.com/*
+
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=greasyfork.org
 // @grant        none
 // ==/UserScript==
